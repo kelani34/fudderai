5efee8e56624a9fc1345e911bfe48310ec05db82
Shina-Kelani Taiwo <kelanitaiwo34@gmail.com>
2024-12-11 15:03:50 +0100

    project init

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,99 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Copy } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="h-screen bg-[url(/bg-cloud.png)] bg-cover bg-no-repeat overflow-hidden">
+      <div className="bg-[#D9D9D9] blur-custom filter w-[150%] ml-[-10%] h-[291px] absolute bottom-0 block md:hidden" />
+      <div className="bg-[url(/bottom-cloud.png)] z-10 bg-bottom bg-contain bg-no-repeat px-8 py-6 h-screen">
+        <div className="flex items-center justify-between">
+          <Image src="/fudderai-logo.png" alt="logo" width={200} height={200} />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="flex gap-[1.29px]">
+            <Link
+              href="https://x.com/Fudder_AI"
+              className={cn(
+                buttonVariants(),
+                "border-l-0 bg-white/50 rounded-l-[14.207px] rounded-r-none p-[12.915px] h-auto"
+              )}
+            >
+              <Image
+                src="/x.svg"
+                width={24.539}
+                height={24.539}
+                alt="Arrow Left"
+              />
+            </Link>
+            <Button className="border-l-0 bg-white/50 rounded-r-[14.207px] rounded-l-none p-[10.671px] h-auto">
+              <Image
+                src="/pumpfun.svg"
+                width={27.78}
+                height={29.433}
+                alt="Arrow Left"
+              />
+            </Button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <div className="flex items-end justify-center md:h-[80vh] h-[62vh] max-w-[806.966px] mx-auto relative">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/fuddai-tv.png"
+            alt="Fudderai"
+            className="z-[150]"
+            width={806.966}
+            height={583}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/dippp.png"
+            alt="Fudderai"
+            width={111.669}
+            height={593}
+            className="absolute right-0 z-[100]"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <div className="absolute bottom-[-32%] w-full flex md:hidden items-center justify-center ">
+            <Image
+              src="/slazzer.png"
+              alt="Fudderai"
+              className="z-10"
+              width={177}
+              height={177}
+            />
+          </div>
+
+          <div className="absolute md:flex hidden  items-center justify-between  bottom-0 md:w-[150%]  ">
+            <Image
+              src="/slazzer.png"
+              alt="Fudderai"
+              className="z-10"
+              width={239}
+              height={239}
+            />
+            <Image
+              src="/slazzer.png"
+              alt="Fudderai"
+              className="z-10"
+              width={239}
+              height={239}
+            />
+          </div>
+        </div>
+        <div className="flex items-center max-w-[541px] mx-auto absolute bottom-6 left-1/2 transform -translate-x-1/2 w-full z-10 px-5">
+          <div className="rounded-l-[15px] bg-[#3C4246] p-[10px] text-2xl text-white">
+            CA:
+          </div>
+          <input
+            className="bg-[#9EB6D1] text-2xl p-[10px] ring-0 w-full"
+            value="2NXaMVsYgcZfeZgRACBTCm"
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <Button className="rounded-l-none rounded-r-[15px] p-[20px] bg-white hover:bg-white/80 h-auto shadow-none ml-[2px]">
+            <Copy color="black" size={34} className="shrink-0 p-[3px]" />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
